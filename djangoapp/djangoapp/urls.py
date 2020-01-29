298cdb4e17d637d2aab03a7ee30f77353b478a41
Robert Morrison <robert.morrison@4-most.co.uk>
2020-01-29 22:30:32 +0000

    Added view & URL to pull shiny HTML. No reactivity allowed.

--- djangoapp/djangoapp/urls.py
+++ djangoapp/djangoapp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shiny/', views.shiny, name=shiny),
+    path('shiny/', views.shiny, name='shiny'),
+    path('shiny_contents/', views.shiny_contents, name='shiny_contents')
 ]
--- djangoapp/djangoapp/views.py
+++ djangoapp/djangoapp/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+
+import requests
+from bs4 import BeautifulSoup
 
 def shiny(request):
     return render(request, 'djangoapp/shiny.html')
+
+
+def shiny_contents(request):
+    response = requests.get("http://localhost:8100")
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return JsonResponse({'html_contents': str(soup)})
